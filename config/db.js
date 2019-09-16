d3fc73091b0240510b571bd770f244f5306aee89
G Mack <mack.gerrit@gmail.com>
2019-09-16 16:32:26 -0700

    third commit from 9/16

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("MongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    });
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(err.message);
+    //exit the process with a failure.
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
