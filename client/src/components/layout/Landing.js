101bc8194d74f5e835978c576dbfe0f71a9cacb1
G Mack <mack.gerrit@gmail.com>
2019-09-20 08:58:17 -0700

    Routers set up, homepage is rendering!

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Bikepackers.net</h1>
+          <p className="lead">
+            Create a bikepacking profile/portfolio, share posts and get advice
+            from other bikepackers
+          </p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-light">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
