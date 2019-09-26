b83bc33e468ff288d370d199db53675ea0746726
G Mack <mack.gerrit@gmail.com>
2019-09-26 16:46:59 -0700

    .

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import ProfileAbout from "./ProfileAbout";
 import Spinner from "../layout/Spinner";
 import ProfileTop from "./ProfileTop";
 import { getProfileById } from "../../actions/profile";
@@ -33,6 +34,7 @@ const Profile = ({
             )}
           <div class="profile-grid my-1">
             <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
           </div>
         </Fragment>
       )}
