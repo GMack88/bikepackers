a50d9be0be588d54fb18ed22efffbfaae08d68dc
G Mack <mack.gerrit@gmail.com>
2019-10-02 14:23:36 -0700

    ready for hosting

---
+++ client/build/precache-manifest.e952bcd62cb57181e2f51a3f97fb7bb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1886de9eb0c2590adea074760d25f3b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "186ea8e41041fc335368",
+    "url": "/static/css/main.e1901cf8.chunk.css"
+  },
+  {
+    "revision": "b3ebe30121e882363569",
+    "url": "/static/js/2.ccd21ee5.chunk.js"
+  },
+  {
+    "revision": "186ea8e41041fc335368",
+    "url": "/static/js/main.b95bb7cf.chunk.js"
+  },
+  {
+    "revision": "2055ac218fe3f7ad47ca",
+    "url": "/static/js/runtime-main.a7ca617d.js"
+  },
+  {
+    "revision": "8f7acc4040e9463dceeda600540f40c0",
+    "url": "/static/media/mountainBiking.8f7acc40.jpg"
+  },
+  {
+    "revision": "514bc3d68dbad00f05858bebe9294d6c",
+    "url": "/static/media/spinner.514bc3d6.gif"
+  }
+]);
--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -7,7 +7,9 @@ export default function(state = initialState, action) {
 
   switch (type) {
     case SET_ALERT:
-      return [...state, payload];
+      const cpy = state.slice();
+      cpy.push(payload);
+      return cpy;
     case REMOVE_ALERT:
       return state.filter(alert => alert.id !== payload);
     default:
